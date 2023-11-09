ba75428357f686dde1934e83a9612cb16162c907
Will Hedgecock <will.hedgecock@gmail.com>
2023-11-09 16:20:38 -0600

    Lecture 18

--- lecture-15/code/main.c
+++ lecture-15/code/main.c
@@ -18,20 +18,17 @@ void exit_with_error(char *msg)
 void client(char *msg)
 {
   int fd;
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    exit_with_error(0);
+
   struct sockaddr_in in_addr;
   memset(&in_addr, 0, sizeof(struct sockaddr_in));
-  
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    exit_with_error(0);
-  }
-
   in_addr.sin_family = AF_INET;
   in_addr.sin_port = htons(5050);
   inet_pton(AF_INET, "127.0.0.1", &in_addr.sin_addr);
 
-  if (connect(fd, (struct sockaddr *)&in_addr, sizeof(struct sockaddr_in))) {
+  if (connect(fd, (struct sockaddr *)&in_addr, sizeof(struct sockaddr_in)))
     exit_with_error("Client error with connect");
-  }
 
   int ret, sent = 0;
   while (sent != strlen(msg)) {
@@ -43,45 +40,41 @@ void client(char *msg)
       sent += ret;
     printf("sent = %d\n", sent);
   }
+
+  close(fd);
 }
 
 void server()
 {
   int fd;
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    exit_with_error("Server error with socket");
+
   struct sockaddr_in in_addr;
   memset(&in_addr, 0, sizeof(struct sockaddr_in));
-  
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    exit_with_error("Server error with socket");
-  }
-
   in_addr.sin_family = AF_INET;
   in_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   in_addr.sin_port = htons(5050); 
 
-  if (bind(fd, (struct sockaddr *) &in_addr, sizeof(struct sockaddr_in))) {
+  if (bind(fd, (struct sockaddr *) &in_addr, sizeof(struct sockaddr_in)))
     exit_with_error("Server error with bind");
-  }
 
-  if (listen(fd, 10)) {
+  if (listen(fd, 10))
     exit_with_error("Server error with listen");
-  }
 
   int client_fd;
   struct sockaddr_storage client_info;
   memset(&client_info, 0, sizeof(struct sockaddr_storage));
   socklen_t client_size = sizeof(struct sockaddr_storage);
   
-  if ((client_fd = accept(fd, (struct sockaddr *) &client_info, &client_size)) == -1) {
+  if ((client_fd = accept(fd, (struct sockaddr *) &client_info, &client_size)) == -1)
     exit_with_error("Server error with accept");
-  }
 
-  int ret, total = 0;
   char buf[100];
+  int ret, total = 0;
   while ((ret = read(client_fd, buf + total, sizeof(buf) - 1 - total))) {
-    if (ret == -1) {
+    if (ret == -1)
       exit_with_error("Server error with read");
-    }
     total += ret;
   }
 
